d9d7f134da773aacb988765d4d41a11f31806625
antegorov <antegorov@mail.ru>
2023-07-07 19:59:15 +0500

    Выполнил 3 задание

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-let title = "My project";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 250.0;
+"use strict";
+
+let title = prompt("Как называется проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = +prompt("Сколько будет стоить данная работа");
 let rollback = 64;
-let fullPrice = 500000;
-let adaptive = true;
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let servicePercentPrice = Math.ceil(fullPrice - fullPrice * 0.03);
+console.log(servicePercentPrice);
+
+fullPrice > 30000
+	? console.log("Даем скидку в 10%")
+	: fullPrice >= 15000 && fullPrice < 30000
+	? console.log("Даем скидку в 5%")
+	: fullPrice < 15000 && fullPrice > 0
+	? console.log("Скидка не предусмотрена")
+	: console.log("Что-то пошло не так");
 
 console.log(typeof title);
 console.log(typeof fullPrice);
