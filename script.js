72cea88f9b174ac3e56906f8d5895a29e152e1c6
antegorov <antegorov@mail.ru>
2023-07-07 10:22:53 +0500

    Выполнил основное задание

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive
-alert('Hello, world !')
-console.log('My console')
+let title = "My project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 250.0;
+let rollback = 64;
+let fullPrice = 500000;
+let adaptive = true;
+/* alert("Hello, world !"); */
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(
+	"Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани"
+);
+
+console.log(screens.toLowerCase().split(", "));
