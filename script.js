14ecd2a2c8dc721478e16e258068c38f4d6deaef
antegorov <antegorov@mail.ru>
2023-07-07 10:25:55 +0500

    merge lesson02 in dev

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let screenPrice = 250.0;
 let rollback = 64;
 let fullPrice = 500000;
 let adaptive = true;
-/* alert("Hello, world !"); */
+
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
