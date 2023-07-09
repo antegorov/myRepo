48f3236f764b7ec6a875a18ce675a908034eb164
antegorov <antegorov@mail.ru>
2023-07-09 12:20:11 +0500

    Выполнил 4 задание

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
-"use strict";
+'use strict'
 
-let title = prompt("Как называется проект?");
-let screens = prompt("Какие типы экранов нужно разработать?");
-let screenPrice = +prompt("Сколько будет стоить данная работа");
-let rollback = 64;
-let adaptive = confirm("Нужен ли адаптив на сайте?");
+const rollback = 10
 
-let service1 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice1 = +prompt("Сколько это будет стоить?");
-let service2 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice2 = +prompt("Сколько это будет стоить?");
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let title = prompt('Как называется проект?')
+let screens = prompt('Какие типы экранов нужно разработать?')
+let screenPrice = +prompt('Сколько будет стоить данная работа')
+let adaptive = confirm('Нужен ли адаптив на сайте?')
+let service1 = prompt('Какой дополнительный тип услуги нужен?')
+let servicePrice1 = +prompt('Сколько это будет стоить?')
+let service2 = prompt('Какой дополнительный тип услуги нужен?')
+let servicePrice2 = +prompt('Сколько это будет стоить?')
 
-let servicePercentPrice = Math.ceil(fullPrice - fullPrice * 0.03);
-console.log(servicePercentPrice);
+let fullPrice, allServicePrices, servicePercentPrice
 
-fullPrice > 30000
-	? console.log("Даем скидку в 10%")
-	: fullPrice >= 15000 && fullPrice < 30000
-	? console.log("Даем скидку в 5%")
-	: fullPrice < 15000 && fullPrice > 0
-	? console.log("Скидка не предусмотрена")
-	: console.log("Что-то пошло не так");
+const showTypeOf = function (variable) {
+	console.log(variable, typeof variable)
+}
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
+const getRollbackMessage = function (price) {
+	if (price >= 30000) return 'Даем скидку в 10%'
+	else if (price >= 15000 && price < 30000) return 'Даем скидку в 5%'
+	else if (price < 15000 && price > 0) return 'Скидка не предусмотрена'
+	else return 'Что-то пошло не так'
+}
+
+const getAllServicePrices = function (sPrice1, sPrice2) {
+	allServicePrices = sPrice1 + sPrice2
+	return allServicePrices
+}
+
+function getFullPrice(price, sPrice) {
+	fullPrice = price + sPrice
+	return fullPrice
+}
+
+function getTitle(str) {
+	str = str.trim()
+	return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+}
+
+const getServicePercentPrices = function (price) {
+	servicePercentPrice = price - price * rollback * 0.01
+	return servicePercentPrice
+}
+
+getTitle(' КаЛьКулятор Верстки')
+getAllServicePrices(servicePrice1, servicePrice2)
+getFullPrice(screenPrice, allServicePrices)
+getServicePercentPrices(fullPrice)
+
+showTypeOf(title)
+showTypeOf(screenPrice)
+showTypeOf(adaptive)
+console.log(screens.toLowerCase().split(', '))
+console.log(getRollbackMessage(fullPrice))
+console.log(servicePercentPrice)
 console.log(
-	"Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани"
-);
-
-console.log(screens.toLowerCase().split(", "));
+	'Стоимость верстки экранов ' + screenPrice + ' рублей/ долларов/гривен/юани'
+)
+//console.log(getTitle(' КаЛьКулятор Верстки'))
