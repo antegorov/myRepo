18edffa86cf0d59e5cc60aba9e63396d209312bf
antegorov <antegorov@mail.ru>
2023-10-21 11:03:19 +0500

    Выполнил 10 задание

--- script.js
+++ script.js
@@ -1,98 +1,83 @@
 'use strict'
 
-const appData = {
-	rollback: 10,
-	title: '',
-	screens: [],
-	screenPrice: 0,
-	adaptive: true,
-	allServicePrices: 0,
-	fullPrice: 0,
-	servicePercentPrice: 0,
-	services: {},
+const book = document.querySelectorAll('.book')
+const body = document.querySelector('body')
 
-	start: function () {
-		appData.asking()
-		appData.addPrices()
-		//appData.getAllServicePrices()
-		appData.getFullPrice()
-		appData.getServicePercentPrices()
-		appData.getTitle()
+let arrayName = []
+let arrayValue = []
+for (let i = 0; i < book.length; i++) {
+	let str = book[i].children[0].textContent
+	arrayName = str.split(' ')
+	let value = arrayName[1].replace('.', '')
+	arrayValue.push(Number(value))
+}
 
-		appData.logger()
-	},
-	logger: function () {
-		console.log(appData.fullPrice)
-		console.log(appData.servicePercentPrice)
-		console.log(appData.screens)
-	},
+let arrayValueSort = arrayValue
+arrayValueSort = arrayValueSort.slice().sort(function (a, b) {
+	return a - b
+})
 
-	asking: function () {
-		appData.title = prompt('Как называется проект?', 'Калькулятор верстки')
+for (let i = 0; i < arrayValueSort.length; i++) {
+	for (let j = 0; j < arrayValueSort.length; j++) {
+		let str = book[j].children[0].textContent
+		arrayName = str.split(' ')
+		let value = arrayName[1].replace('.', '')
+		value = Number(value)
 
-		for (let i = 0; i < 2; i++) {
-			let name = prompt('Какие типы экранов нужно разработать?')
-			let price = 0
-
-			do {
-				price = prompt('Сколько будет стоить данная работа')
-			} while (!appData.isNumber(price))
-
-			appData.screens.push({ id: i, name: name, price: price })
+		if (value == arrayValueSort[i]) {
+			// debugger
+			book[i].parentNode.insertAdjacentElement('beforeend', book[j])
+			break
 		}
-
-		for (let i = 0; i < 2; i++) {
-			let name = prompt('Какой дополнительный тип услуги нужен?')
-			let price
-
-			while (!appData.isNumber(price)) {
-				price = prompt('Сколько это будет стоить?')
-			}
-
-			let str = i + '_' + name
-			appData.services[str] = +price
-		}
-
-		appData.adaptive = confirm('Нужен ли адаптив на сайте?')
-	},
-
-	addPrices: function () {
-		appData.screenPrice = appData.screens.reduce(function (summary, item) {
-			return +item.price + summary
-		}, 0)
-
-		for (let key in appData.services) {
-			appData.allServicePrices += appData.services[key]
-		}
-	},
-
-	isNumber: function (num) {
-		return !isNaN(parseFloat(num)) && isFinite(num) && num !== null && num != ''
-	},
-	getRollbackMessage: function (price) {
-		if (price >= 30000) return 'Даем скидку в 10%'
-		else if (price >= 15000 && price < 30000) return 'Даем скидку в 5%'
-		else if (price < 15000 && price >= 0) return 'Скидка не предусмотрена'
-		else return 'Что-то пошло не так'
-	},
-	// getAllServicePrices: function () {
-	// 	for (let key in appData.services) {
-	// 		appData.allServicePrices += appData.services[key]
-	// 	}
-	// },
-	getFullPrice: function () {
-		appData.fullPrice = +appData.screenPrice + appData.allServicePrices
-	},
-	getTitle: function () {
-		appData.title = appData.title.replace(/^ +/, '')
-		appData.title =
-			appData.title.charAt(0).toUpperCase() +
-			appData.title.slice(1).toLowerCase()
-	},
-	getServicePercentPrices: function () {
-		appData.servicePercentPrice =
-			appData.fullPrice - appData.fullPrice * appData.rollback * 0.01
 	}
 }
 
-appData.start()
+// 2
+body.style.backgroundImage = "url('image/you-dont-know-js.jpg')"
+
+// 3
+for (let i = 0; i < arrayValueSort.length; i++) {
+	let str = book[i].children[0].textContent
+	arrayName = str.split(' ')
+	let value = arrayName[1].replace('.', '')
+	value = Number(value)
+	if (value == 3) {
+		book[i].querySelector('a').innerHTML = 'Книга 3. this и Прототипы Объектов'
+		break
+	}
+}
+
+const adv = document.querySelector('.adv')
+adv.remove()
+
+for (let i = 0; i < arrayValueSort.length; i++) {
+	let str = book[i].children[0].textContent
+	arrayName = str.split(' ')
+	let value = arrayName[1].replace('.', '')
+	value = Number(value)
+	if (value == 2) {
+		let list = book[i].querySelectorAll('li')
+		list[2].before(list[3])
+		list[3].after(list[6])
+		list[2].after(list[8])
+		list[9].after(list[2])
+	}
+
+	if (value == 5) {
+		let list = book[i].querySelectorAll('li')
+		list[1].after(list[9])
+		list[2].before(list[3])
+		list[3].after(list[4])
+		list[8].before(list[5])
+		break
+	}
+	//Глава 8: За пределами ES6
+	if (value == 6) {
+		let list = book[i].querySelectorAll('li')
+		const newElem = document.createElement('li')
+		newElem.textContent = 'Глава 8: За пределами ES6'
+		list[0].parentNode.append(newElem)
+		// console.log(list[9].textContent)
+		list[9].parentNode.append(list[9])
+	}
+}
