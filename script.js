55701c1d2ce346f2ba2195fc71fd1483e9d83555
antegorov <antegorov@mail.ru>
2023-11-12 10:12:55 +0500

    Выполнил 11 задание

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-'use strict'
+const btn = document.getElementById('btn')
+const input = document.getElementById('text')
+const range = document.getElementById('range')
+const btnCircle = document.getElementById('e_btn')
+const square = document.getElementById('square')
+
+let color = ''
+
+const getColor = function (e) {
+	color = e.target.value
+}
+
+input.addEventListener('change', getColor)
+
+btn.addEventListener('click', function () {
+	square.style.backgroundColor = color
+})
+
+range.addEventListener('input', function (e) {
+	circle.style.width = e.target.value + '%'
+	circle.style.height = e.target.value + '%'
+})
+
+btnCircle.style.display = 'none'
